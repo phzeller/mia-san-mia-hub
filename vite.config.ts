0c5d07bbb2b514622286fc5c37fc64f7c95087e1
phzeller <i.am@philippzeller.com>
2025-07-27 18:16:04 +0200

    removed base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/mia-san-mia-hub/" : "./", // Use repository name for production
   server: {
     host: "::",
     port: 8080,
